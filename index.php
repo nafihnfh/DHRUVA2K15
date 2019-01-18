8f5a3dde5bd92922631e721842e75479e40b93c3
nafih c <nafihnfh5@gmail.com>
2019-01-18 22:31:04 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,250 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8"><title>DHRUVA 2K15</title>
+		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+		<meta name="description" content="" />
+		<meta name="keywords" content="" />
+		<script src="js/jquery.min.js"></script>
+		<script src="js/skel.min.js"></script>
+		<script src="js/skel-layers.min.js"></script>
+		<script src="js/init.js"></script>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+        <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+	
+			<link rel="stylesheet" href="css/skel.css" />
+			<link rel="stylesheet" href="css/style.css" />
+                <link rel="stylesheet" href="css/main.css" />
+	</head>
+
+	<body class="landing">
+
+		<!-- Header -->
+			<header id="header">
+				<h1><a href="#">Brochure</a></h1>
+				<nav top fixed id="nav">
+					<ul>
+						<li><a href="#banner">Home</a></li>
+			<li><a href="#one">Technical Events</a></li>
+						<li><a href="#two">General Events</a></li>
+                <li><a href="#footer">Contact</a></li>
+						<li><a href="registration.php" class="button special">Registration</a></li>
+					</ul>
+				</nav>
+			</header>
+
+		<!-- Banner -->
+			<section id="banner">	
+			<ul class="actions">
+					<li>
+<img src ="images/logo.png"></img>
+</li>
+				</ul>
+                          <h2>DHRUVA 2K15</h2>
+    				 <p>Dare To Dream Beyond.</p>
+                  <p style="font-size:150%">OCTOBER 16-17th</p>
+                      <h3 style="color:white">NSS COLLEGE OF ENGINEERING</h3>
+                     <h4 style="color:white">ELECTRONICS AND COMMUNICATION</h4>
+
+		</section>
+
+<section id="scrolling">
+ <div class="row " style="background:#fff;"><div class="col-md-1">&nbsp; </div><div class="col-md-10">
+<marquee style="overflow: hidden;white-space: nowrap;"><h4 style="color:red;" class="text-danger">DUBSMASH contest  whatsapp your video by 10/10/2015 to 7025839098  9847404796 &nbsp;&nbsp;&nbsp; |&nbsp;&nbsp;&nbsp;<span> </span>&nbsp;&nbsp;&nbsp; |&nbsp;&nbsp;&nbsp;<span> </span>Hurry
+ Up!!!! Registration closes on 10th october 2015 . &nbsp;&nbsp;&nbsp; |&nbsp;&nbsp;&nbsp;  welcome to dhruva Nss college of engineering </h4></marquee></div></div>
+                
+</section>
+
+
+		<!-- One -->
+<section id="one" class="wrapper style1 special">
+<div class="container">
+<header class="major">
+<h2>TECHNICAL EVENTS</h2>
+</header>
+
+<div class="row 150%">
+<div class="4u 12u$(medium)">
+<section class="box">
+<div class="image fit">
+<a href="kurukshetra.php">
+<img src ="images/KURUKSHETRA.png"></img>
+</div>
+<h3>KURUKSHETRA</a></h3>
+<h4><b>Win a cash award of Rs 20K</b></h4>
+</section>
+</div>
+						
+<div class="4u 12u$(medium)">
+<section class="box">
+<div class="image fit">
+<a href="agneepath.php">
+<img src ="images/AGNEEPATH.png"></img>
+</div>
+<h3>AGNEEPATH</a></h3>
+<h4><b>Win a cash award of Rs 15K</b></h4>
+</section>
+</div>
+
+<div class="4u 12u$(medium)">
+<section class="box">
+<div class="image fit">
+<a href="pathmavyuha.php">
+<img src ="images/PATHMAVYUHA.png"></img>
+</div>
+<h3>PATHMAVYUHA</a></h3>
+<h4><b>Win a cash award of Rs 12K</b></h4>
+</section>
+</div>
+
+<div class="4u 12u$(medium)">
+<section class="box">
+<div class="image fit">
+<a href="tripura.php">
+<img src ="images/TRIPURA.png"></img>
+</div>
+<h3>TRIPURA</a></h3>
+<h4><b>Win a cash award of Rs 6K</b></h4>
+</section>
+</div>
+
+<div class="4u 12u$(medium)">
+<section class="box">
+<div class="image fit">
+<a href="mr_electronics.php">
+<img src ="images/MR. ELECTRONICS.png"></img>
+</div>
+<h3>MR. ELECTRONICS</h3></a>
+<h4><b>Win a cash award of Rs 10K</b></h4>
+</section>
+</div>
+
+<div class="4u$ 12u$(medium)">
+<section class="box">
+<a href="find_and_diffuse.php">
+<div class="image fit">
+<img src ="images/FIND AND DIFFUSE.png"></img>
+</div>
+<h3>FIND AND DIFFUSE</h3></a>
+<h4><b>Win a cash award of Rs 8K</b></h4>
+</section>
+</div>
+
+</div>
+</div>
+</section>
+
+<!-- Two -->
+
+<section id="two" class="wrapper style1 special">
+<div class="container">
+<header class="major">
+<h2>GENERAL EVENTS</h2>
+</header>
+<div class="row 150%">
+<div class="4u 12u$(medium)">
+<section class="box">
+<a href="tricky_tracker.php">
+<div class="image fit">
+<img src ="images/TRICKY TRACKER.png"></img>
+</div>
+<h3>TRICKY TRACKER</h3></a>
+<h4><b>Win a cash award of Rs 7K</b></h4>
+</section>
+</div>
+						
+<div class="4u 12u$(medium)">
+<section class="box">
+<a href="psuedo_holmes.php">
+<div class="image fit">
+<img src ="images/PSUEDO HOLMES.png"></img></div>
+<h3>PSUEDO HOLMES</h3></a>
+<h4><b>Win a cash award of Rs 8K</b></h4>
+
+</div>
+
+<div class="4u 12u$(medium)">
+<section class="box">
+<div class="image fit">
+<a href="re_ignite.php">
+<img src ="images/RE-IGNITE.png"></img>
+</div>
+<h3>RE-IGNITE</h3></a>
+<h4><b>Win a cash award of Rs 10K</b></h4>
+</section>
+</div>
+
+
+<div class="4u 12u$(medium)">
+<section class="box">
+<a href="aswamedha.php">
+<div class="image fit">
+<img src ="images/ASWAMEDHA.png"></img>
+</div>
+<h3>ASWAMEDHA</h3></a>
+<h4><b>Win a cash award of Rs 8K</b></h4>
+
+</section>
+</div>
+
+<div class="4u 12u$(medium)">
+<section class="box">
+<a href="ranabhoomi.php">
+<div class="image fit">
+<img src ="images/RANABHOOMI.png"></img>
+</div>
+<h3>RANABHOOMI</h3></a>
+<h4><b>Win a cash award of Rs 12K</b></h4>
+
+
+</section>
+</div>
+
+<div class="4u$ 12u$(medium)">
+<section class="box">
+<a href="football.php">
+<div class="image fit">
+<img src ="images/FOOTBALL.png"></img>
+</div>
+<h3>3'S FOOTBALL</h3></a>
+<h4><b>Win a cash award of Rs 4K</b></h4>
+
+
+</section>
+</div>
+
+
+
+
+</div>
+</div>
+</section>
+
+		<!-- Three -->
+
+<section id="three" class="wrapper style3 special">
+<section class="container">
+<header class="major">
+<h2>DHRUVA 2K15 Teaser</h2>
+<div class="row">
+<div class="com-md-12">
+<iframe width="1150" height="680" src="https://www.youtube.com/embed/uKZ5QcJB6qI" frameborder="10" allowfullscreen>
+</div>
+</iframe>
+
+	
+</header>
+
+   </div></div>
+			</section>
+
+
+
+      <!-- Footer -->
+	
+ <?php include 'footer.php';?>
+        </style>
+	</body>
+</html>
